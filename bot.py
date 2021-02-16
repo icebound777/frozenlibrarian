d8cc53caff2ce088f31af625d270a3d78e4e3f32
Icebound_IB7 <frozenblue@gmx.de>
2021-02-16 23:48:26 +0100

    pylint module docstrings

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 # bot.py
+"""Chat bot for the discord chat software"""
 import os
-
 import discord
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,6 +12,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    """Initial actions after starting up"""
     print(f'{client.user} has connected to Discord!')
 
 client.run(TOKEN)
